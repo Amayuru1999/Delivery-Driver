b2556370cca200f337a2e7c3f2a52c9c5051fe07
Amayuru1999 <amarasingheau@gmail.com>
2024-09-06 00:15:36 +0530

    feat: added cutomer delivery pick up with hasPackage bool

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage;
+    
     void OnCollisionEnter2D(Collision2D other)
     {
        Debug.Log("❌ Collision detected with " + other.gameObject.name);
@@ -14,11 +16,13 @@ public class Delivery : MonoBehaviour
         if (other.tag == "Package")
         {
             Debug.Log("📦 Package picked up " + other.gameObject.name);
+            hasPackage = true;
             
         }
-        if(other.tag == "Customer")
+        if(other.tag == "Customer" && hasPackage)
         {
             Debug.Log("📦 Package delivered to "+ other.gameObject.name);
+            hasPackage=false;
         }
     }
 }
