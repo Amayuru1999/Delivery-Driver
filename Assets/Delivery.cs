697bbd5c83f277b252fce6900292467d3ac4fc73
Amayuru1999 <amarasingheau@gmail.com>
2024-09-06 14:57:13 +0530

    feat: added boost speed for the car

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -5,19 +5,30 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField]  Color32 hasPackageColor = new Color32(1,1,1,1);
+    [SerializeField]  Color32 noPackageColor = new Color32(1, 1, 1, 1);
+    
     [SerializeField]  float destroyDelay = 0.5f;
     bool hasPackage;
-    
+
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     void OnCollisionEnter2D(Collision2D other)
     {
        Debug.Log("❌ Collision detected with " + other.gameObject.name);
     }
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Package")
+        if (other.tag == "Package" && !hasPackage)
         {
             Debug.Log("📦 Package picked up " + other.gameObject.name);
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
             Destroy(other.gameObject,destroyDelay);
             
         }
@@ -25,6 +36,7 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("📦 Package delivered to "+ other.gameObject.name);
             hasPackage=false;
+            spriteRenderer.color = noPackageColor;
         }
     }
 }
--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,14 +6,10 @@ using UnityEngine;
 public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed=1;
-    [SerializeField] float moveSpeed=0.01f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] float moveSpeed=20f;
+    [SerializeField] float slowSpeed=15f;
+    [SerializeField] float boostSpeed=30f;
+    
     void Update()
     {
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
@@ -20,5 +17,18 @@ public class Driver : MonoBehaviour
         transform.Rotate(0,0,-steerAmount);
         transform.Translate(0,moveAmount,0);
        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        moveSpeed = slowSpeed;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Boost")
+        {
+            moveSpeed = boostSpeed;
+        }
     }
 }
